e003270a8d4a88ac27f8da03bca27f490bfd994a
Navya-Kamma <navyakamma1512@gmail.com>
2024-09-19 15:35:16 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,13 @@
+const timeElement = document.getElementById('time');
+
+function updateClock() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  timeElement.textContent = timeString;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
